678554d6cd9d9af1bd0e497ebbf523b4a1217cd9
haipt-05 <Hai.PT235701@sis.hust.edu.vn>
2025-04-03 10:42:58 +0700

    Create DigitalVideoDisc.java

---
+++ DigitalVideoDisc.java
@@ -0,0 +1,25 @@
+package Aims;
+public class DigitalVideoDisc {
+	private String title;
+	private String category;
+	private String director;
+	private int lenght;
+	private float cost;
+	public String getTitle() {
+		return title;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public int getLenght() {
+		return lenght;
+	}
+	public float getCost() {
+		return cost;
+	}
+	
+	
+}
