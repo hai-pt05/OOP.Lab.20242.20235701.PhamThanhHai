96aab0e36dfa2fdf49cbb370c29a117e6e4db268
haipt-05 <Hai.PT235701@sis.hust.edu.vn>
2025-04-03 10:46:23 +0700

    Update DigitalVideoDisc.java

--- DigitalVideoDisc.java
+++ DigitalVideoDisc.java
@@ -3,8 +3,38 @@ public class DigitalVideoDisc {
 	private String title;
 	private String category;
 	private String director;
-	private int lenght;
+	private int length;
 	private float cost;
+	
+	//1. Chỉ dùng title
+    public DigitalVideoDisc(String title) {
+        this.title = title;
+    }
+    
+    //2. Dùng category, title và cost
+    public DigitalVideoDisc(String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+    }
+    
+    //3. Dùng director, category, title và cost
+    public DigitalVideoDisc(String director, String category, String title, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.cost = cost;
+    }
+    
+    //4. Dùng tất cả thuộc tính
+    public DigitalVideoDisc(String title, String category, String director, int length, float cost) {
+        this.title = title;
+        this.category = category;
+        this.director = director;
+        this.length = length;  
+        this.cost = cost;
+    }
+	
 	public String getTitle() {
 		return title;
 	}
@@ -14,12 +44,12 @@ public class DigitalVideoDisc {
 	public String getDirector() {
 		return director;
 	}
-	public int getLenght() {
-		return lenght;
+	public int getLength() {
+		return length;
 	}
 	public float getCost() {
 		return cost;
 	}
 	
-	
+
 }
