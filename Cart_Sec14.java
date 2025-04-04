7ca11c6cf40099cccb3fd6bb1a4b2fab5b9b9e7a
haipt-05 <Hai.PT235701@sis.hust.edu.vn>
2025-04-04 10:18:25 +0700

    Update Cart_Sec14 - Overloading

--- Cart_Sec14.java
+++ Cart_Sec14.java
@@ -1,6 +1,6 @@
 package Aims;
 
-public class Cart {
+public class Cart_Sec14 {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
